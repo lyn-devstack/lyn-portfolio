1442d04c288fcce22a54be28c34805c47de98745
lyn-devstack <carolyn.logic@gmail.com>
2026-04-23 00:17:31 +0200

    fix: ajustar base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/lyn-portfolio/', // <--- Asegúrate de que este sea el nombre exacto de tu repo en GitHub
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
